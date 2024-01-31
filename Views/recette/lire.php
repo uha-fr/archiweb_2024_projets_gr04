c67a7b29fec074dc7d13177a426f6d1c7e87d444
TheoNicod <theonicod@hotmail.fr>
2024-01-31 11:19:20 +0100

    Gestion des recettes + mon frigo

--- Views/recette/lire.php
+++ Views/recette/lire.php
@@ -1,3 +1,11 @@
 <h2>La recette en détail</h2>
 <p><?= $recette->nom ?></p>
 <p><?= $recette->description ?></p>
+
+<h2>Ingrédients</h2>
+<ul>
+<?php foreach($ingredients as $ingredient): ?>
+    <li><?= $ingredient->nom ?></li>
+<?php endforeach ?>
+</ul>
+
