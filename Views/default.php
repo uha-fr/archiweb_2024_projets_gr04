9be0c241c0a8928bfdc66d52bee53d64e25055b7
ellouzatim <ellouzati.mohamed84@gmail.com>
2024-02-11 11:16:26 +0100

    synch

--- Views/default.php
+++ Views/default.php
@@ -4,9 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="/css/styles.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <nav>
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+        <div class="container px-5">
+            <a class="navbar-brand" href="#page-top">MANGER</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="">Aliment</a></li>
+                    <li class="nav-item"><a class="nav-link" href="">Recette</a></li>
+                    <li class="nav-item"><a class="nav-link" href="">Planing</a></li>
+                    <li class="nav-item sign-in-up"><a class="nav-link" href="">Inscription</a></li>
+                    <li class="nav-item sign-in-up"><a class="nav-link" href="">Connexion</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <nav class="mt-5 pt-4">
         Menu Recettes
         <?php
         if (session_status() !== PHP_SESSION_ACTIVE) {
@@ -15,15 +32,18 @@
 
         if(isset($_SESSION['utilisateur'])) : ?>
             <span>Bienvenue, <?= $_SESSION['utilisateur']['nom_utilisateur'] ?></span>
-            <a href="utilisateur/logout">Déconnexion</a>
+            <a href="/utilisateur/logout">Déconnexion</a>
         <?php else : ?>
-            <a href="utilisateur/login">Connexion</a>
+            <a href="/utilisateur/login">Connexion</a>
         <?php endif; ?>
     </nav>
-    <div class="conteneur"><?= $contenu ?></div>
+
+    <div class="conteneur mt-5"> 
+        <?= $contenu ?>
+    </div>
 
     <script type="text/javascript">console.log("default template");</script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-1CmRxJ+EN6gDNtYKb/E/fWd0LbRc5zNU6cP4U9d8jJHONaU7eQc7Jm6yR03b1z7b" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ Views/planning/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calendrier de Planification</title>
+    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/core@6.1.10/index.global.min.js'></script>
+    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@6.1.10/index.global.min.js'></script>
+    
+        <script src="public/src/calendrier.js"></script>
+    </head>
+    <body>
+        <?php include '../default.php'; ?>
+        <div id='calendar'></div>
+    </body>
+</html>
+    
